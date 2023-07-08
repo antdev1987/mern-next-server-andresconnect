cd7b725669b34b25907b31fdb4ac7be35697697e
nicolle <antdev1987@gmail.com>
2023-07-08 03:37:24 -0600

    register

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,91 +1,130 @@
-import express from 'express';
-import bcryptjs from 'bcryptjs';
-import User from '../models/userModel.js';
+import express from "express";
+import bcryptjs from "bcryptjs";
+import User from "../models/userModel.js";
 // import jwt from 'jsonwebtoken';
 
-import { generateToken } from '../utils.js';
+import { generateToken } from "../utils.js";
 
 const userRouter = express.Router();
 
+userRouter.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
 
 
-userRouter.post('/register', async (req, res) => {
+
   try {
-    const data = req.body;
+    const isUserDb = await User.findOne({ email });
 
-    console.log(data);
-
-    const existUser = await User.findOne({ email: data.email });
-
-    if (existUser) {
-      return res.status(500).json({ ok: false, msg: 'El correo ya existe' });
+    //checking if user already exist
+    if (isUserDb) {
+      return res.status(400).json({ message: "User already Registered" });
     }
 
-    // encriptar password
-    data.password = await bcrypt.hash(data.password, 10);
+    const newUser = new User({
+      name,
+      email,
+      password: password != "" ? bcryptjs.hashSync(password) : "",
+    });
 
-    // Guardar usuario
-    const newUser = await User.create(data);
+    // newUser.token =
+    //   Math.random().toString(32).substring(2) + Date.now().toString(32);
 
-    // Creando Token
-    const token = jwt.sign({ id: newUser._doc._id }, process.env.SIGN);
+    await newUser.save();
 
-    delete newUser._doc.password;
-    delete newUser._doc._id;
+    
 
     res.json({
-      ok: true,
-      msg: 'El usuario fue creado exitosamente',
-      data: {
-        token,
-        ...newUser._doc,
-      },
+      message:
+        "User succesfully created, please login",
     });
+
   } catch (error) {
-    res.status(500).json({ ok: false, msg: error.message });
+    console.log(error);
+    //to check mongoose validation error like empty data
+    if (error.name === "ValidationError") {
+      let errors = [];
+
+      Object.keys(error.errors).forEach((key) => {
+        //   errors[key] = error.errors[key].message;
+        errors.push(error.errors[key].message);
+      });
+      return res.status(400).send({ message: errors.join(" ||| ") });
+    }
+
+    res.status(500).send({ message: "Something went wrong" });
   }
+
+  // try {
+  //   const data = req.body;
+
+  //   console.log(data);
+
+  //   const existUser = await User.findOne({ email: data.email });
+
+  //   if (existUser) {
+  //     return res.status(500).json({ ok: false, msg: 'El correo ya existe' });
+  //   }
+
+  //   // encriptar password
+  //   data.password = await bcrypt.hash(data.password, 10);
+
+  //   // Guardar usuario
+  //   const newUser = await User.create(data);
+
+  //   // Creando Token
+  //   const token = jwt.sign({ id: newUser._doc._id }, process.env.SIGN);
+
+  //   delete newUser._doc.password;
+  //   delete newUser._doc._id;
+
+  //   res.json({
+  //     ok: true,
+  //     msg: 'El usuario fue creado exitosamente',
+  //     data: {
+  //       token,
+  //       ...newUser._doc,
+  //     },
+  //   });
+  // } catch (error) {
+  //   res.status(500).json({ ok: false, msg: error.message });
+  // }
 });
 
-userRouter.post('/logIn', async (req, res) => {
+//login endpoint
+userRouter.post("/logIn", async (req, res) => {
+  const { email, password } = req.body;
+  console.log("login route");
 
+  try {
+    const userDb = await User.findOne({ email });
 
-  const { email, password } = req.body
-	console.log('login route')
+    //check if user exist
+    if (!userDb) {
+      return res.status(404).json({ message: "User Does Not Exist" });
+    }
 
-	try {
+    // //check if user is verified
+    // if (!userDb.isVerified) {
+    // 	return res.status(400).json({ message: 'Your Accounts has not been verified' })
+    // }
 
-		const userDb = await User.findOne({ email })
+    //check if password match
+    if (!bcryptjs.compareSync(password, userDb.password)) {
+      return res.status(401).send({ message: "Email or Password Wrong" });
+    }
 
-		//check if user exist
-		if (!userDb) {
-			return res.status(404).json({ message: 'User Does Not Exist' })
-		}
+    const userAuthenticated = {
+      _id: userDb._id,
+      email: userDb.email,
+      name: userDb.name,
+      isAdmin: userDb.isAdmin,
+      token: generateToken(userDb),
+    };
 
-
-		// //check if user is verified
-		// if (!userDb.isVerified) {
-		// 	return res.status(400).json({ message: 'Your Accounts has not been verified' })
-		// }
-
-		//check if password match
-		if (!bcryptjs.compareSync(password, userDb.password)) {
-			return res.status(401).send({ message: 'Email or Password Wrong' })
-		}
-
-		const userAuthenticated = {
-			_id: userDb._id,
-			email: userDb.email,
-			name: userDb.name,
-			isAdmin: userDb.isAdmin,
-			token: generateToken(userDb)
-		}
-
-		res.json(userAuthenticated)
-
-	} catch (error) {
-		console.log(error)
-	}
-
+    res.json(userAuthenticated);
+  } catch (error) {
+    console.log(error);
+  }
 
   // try {
   //   const data = req.body;
