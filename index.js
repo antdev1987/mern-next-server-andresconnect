3e37f60465c6056cd9848bd54637caf05eb79d25
nicolle <antdev1987@gmail.com>
2023-07-27 20:02:02 -0600

    trabando en api de userverification

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 import express from 'express';
 import cors from 'cors';
 import * as dotenv from 'dotenv';
+import 'dotenv/config'
+
 import mongoose from 'mongoose';
 
-// const express = require('express')
-// const cors = require('cors')
-// const dotenv = require('dotenv');
-// const mongoose = require('mongoose')
+import multer from 'multer';
+
+
 
 
 const app = express();
-// dotenv.config();
+
 
 
 app.use(cors())
@@ -26,7 +27,32 @@ app.use(express.json());
 app.use(cors({ origin: '*' }));
 dotenv.config();
 
-// Routes
+
+/* 
+    code bellow is just to handle multer errors so that,
+    you can show nices custom error to client if one of these error occurs
+*/
+app.use((err, req, res, next) => {
+  if (err instanceof multer.MulterError) {
+    if (err.code === "LIMIT_FILE_SIZE") {
+      return res
+        .status(400)
+        .json({ message: "file size too large (2 MB + not allowed)" });
+    }
+
+    if (err.code === "LIMIT_FILE_COUNT") {
+      return res
+        .status(400)
+        .json({ message: "just 3 files per operation is allowed" });
+    }
+
+    if (err.code === "LIMIT_UNEXPECTED_FILE") {
+      return res.status(400).json({ message: "format file not supported" });
+    }
+  }
+});
+
+// Routes of the application
 app.use('/api/user', userRouter);
 
 const port = process.env.PORT || 4000
